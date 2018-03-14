9374ddac8cd900d2e81253c68cb394dbfea99ea9
mivanov <mivanov@cinimex.ru>
2018-03-14 19:21:23 +0300

    feat(app): Default strategy

--- src/app/tree/tree/tree.component.ts
+++ src/app/tree/tree/tree.component.ts
@@ -1,4 +1,4 @@
-import {ChangeDetectionStrategy, Component, Injector} from '@angular/core';
+import {Component, Injector} from '@angular/core';
 import {Lifecycle} from "../../utils/Lifecycle";
 import {TreeNode} from "../base.class";
 
@@ -6,8 +6,7 @@ import {TreeNode} from "../base.class";
 @Component({
   selector: 'tree',
   templateUrl: './tree.component.html',
-  styleUrls: ['./tree.component.css'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  styleUrls: ['./tree.component.css']
 })
 export class TreeComponent extends TreeNode {
 
