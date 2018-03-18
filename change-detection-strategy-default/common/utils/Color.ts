8f61eabf82675f962f86cac6883f70b97329972b
mivanov <mivanov@cinimex.ru>
2018-03-18 20:49:20 +0300

    feat(second): remove unusage
    (cherry picked from commit 9d390f7)

--- change-detection-strategy-default/common/utils/Color.ts
+++
@@ -1,5 +0,0 @@
-export class Color {
-  static generateRGB(color: string = null) {
-    return color || `rgb(${Math.floor(Math.random() * 255)}, ${Math.floor(Math.random() * 255)}, ${Math.floor(Math.random() * 255)})`;
-  }
-}
